4a953d2298aca7ac00a23caa5d88fe124cecf897
kimikanna <svartirose@gmail.com>
2024-08-12 18:49:27 +0300

    Solved JS practice

--- first-steps/what-print-in-console.js
+++ first-steps/what-print-in-console.js
@@ -6,40 +6,41 @@ const obj2 = obj1;
 
 obj2.x = 8;
 
-console.log(obj1.x)
+console.log(obj1.x) // 8
 
 let a = 5;
 let b = a;
 
 b = 9;
 
-console.log(a);
-console.log(b);
+console.log(a); // 5
+console.log(b); // 9
 
 //Приведение типов примитивов
-console.log(true + false)
-console.log(12 / "6")
-console.log(12 < "6")
-console.log("number" + 15 + 3)
-console.log(15 + 3 + "number")
-console.log("foo" + +"bar")
-console.log('true' == true)
-console.log(!!"false" == !!"true")
-console.log(false == 'false')
-console.log(null == '')
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
-console.log(null == undefined)
-console.log(null === undefined)
+console.log(true + false) // 1
+console.log(12 / "6") // 2
+console.log(12 < "6") // false
+console.log("number" + 15 + 3) // number153
+console.log(15 + 3 + "number") // 18number
+console.log("foo" + +"bar") // foo NaN
+console.log('true' == true) // false
+console.log(!!"false" == !!"true") // true
+console.log(false == 'false') // false
+console.log(null == '') // false
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) // true
+console.log(null == undefined) // true
+console.log(null === undefined) // false
 
 
 //Приведение типов с объектами
-console.log([1] > null)
-console.log([1, 2] > null)
-console.log(['x'] == 'x')
-console.log([] + null + 1)
-console.log([1, 2, 3] == [1, 2, 3])
-console.log([1, 2, 3] === [1, 2, 3])
-console.log({ a: 1 } - 1)
-console.log({ a: 1 } + 2)
+console.log("---")
+console.log([1] > null) // true ? ---
+console.log([1, 2] > null) // false ? ---
+console.log(['x'] == 'x') // true ? ---
+console.log([] + null + 1) // null1
+console.log([1, 2, 3] == [1, 2, 3]) // false
+// console.log([1, 2, 3] === [1, 2, 3]) // always false
+console.log({ a: 1 } - 1) // NaN
+console.log({ a: 1 } + 2) // [object Object]2 ? ---
